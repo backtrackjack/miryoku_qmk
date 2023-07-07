9d9faf276d0132e557db8a8fc817e3776e23d10c
Jack Sellars <jsellars@enee.com.au>
2023-07-08 00:30:52 +0930

    feat: bye algr

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -27,13 +27,13 @@
 #define BASE_DVORAK \
 KC_QUOT,       KC_COMM,         KC_DOT,         KC_P,             KC_Y,       KC_F,       KC_G,            KC_C,             KC_R,            KC_L,            \
 HOME_A,        HOME_O,          HOME_E,         HOME_U,           KC_I,       KC_D,       HOME_H,          HOME_T,           HOME_N,          HOME_S,          \
-LBTN_LYR,      ALGR_T(KC_Q),    KC_J,           KC_K,             KC_X,       KC_B,       KC_M,            KC_W,             ALGR_T(KC_V),    RBTN_LYR,        \
+LBTN_LYR,       KC_Q,    KC_J,           KC_K,             KC_X,       KC_B,       KC_M,            KC_W,              KC_V,    RBTN_LYR,        \
 U_NP,          U_NP,            LTHUMB_IN,      LTHUMB_MID,       LTHUMB_OUT, RTHUMB_OUT, RTHUMB_MID,      RTHUMB_IN,        U_NP,            U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 KC_Q,          KC_W,            KC_E,           KC_R,             KC_T,       KC_Y,       KC_U,            KC_I,             KC_O,            KC_P,            \
 LGUI_T(KC_A),  LALT_T(KC_S),    LCTL_T(KC_D),   LSFT_T(KC_F),     KC_G,       KC_H,       LSFT_T(KC_J),    LCTL_T(KC_K),     LALT_T(KC_L),    LGUI_T(KC_QUOT), \
-LT(U_BUTTON,   KC_Z),           ALGR_T(KC_X),   KC_C,             KC_V,       KC_B,       KC_N,            KC_M,             KC_COMM,         ALGR_T(KC_DOT),  LT(U_BUTTON,KC_SLSH), \
+LT(U_BUTTON,   KC_Z),            KC_X,   KC_C,             KC_V,       KC_B,       KC_N,            KC_M,             KC_COMM,          KC_DOT,  LT(U_BUTTON,KC_SLSH), \
 U_NP,          U_NP,            LT(U_MEDIA,     KC_ESC),          LT(U_NAV,   KC_SPC),    LT(U_MOUSE,      KC_TAB),          LT(U_SYM,        KC_ENT),         LT(U_NUM,   KC_BSPC), LT(U_FUN,KC_DEL), U_NP, U_NP
 
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTY \
